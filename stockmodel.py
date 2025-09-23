57d2411f39119ea833ab258c0aa2ca8da914d9f8
kedamonokokyeu <163675358+kedamonokokyeu@users.noreply.github.com>
2025-09-23 16:16:56 -0700

    Update stockmodel.py

--- stockmodel.py
+++ stockmodel.py
@@ -33,6 +33,8 @@ def OHLCV_data_analyzer(): # here we find what's important from OHLCV data
     # average volume
     df["AvgVolume_3m"] = df["Volume"].rolling(63).mean()
 
+    return df
+
 def valuation_ratios():
     pe_ratio = ticker.info.get("trailingPE")
 
@@ -48,9 +50,69 @@ def valuation_ratios():
     current_ratio = balance_sheet["Total Current Assets"].iloc[-1] /  balance_sheet["Total Current Liabilities"].iloc[-1]
     dividend_yield = ticker.info.get("dividendYield")
 
-    return df  
+    return pe_ratio, revenue_growth, profit_margin, debt_equity, dividend_yield
 
-df = OHLCV_data_analyzer()
+#RSI: measures the speed and change of price movements --> a high value suggest a stock is overbought, a low value suggest its oversold
+def rsi_calculation(data, window = 21): # usually 14, but we want long term so...
+    close_difference = data['Close'].diff()
 
-# display last five rows, we only get NaN for the first 200 because those have no past days to "roll" back upon
-print(df.tail())
+    gain = close_difference.where(close_difference > 0, 0) # we need average gain / average loss to calculate RSI line
+    loss = -close_difference.where(close_difference < 0, 0) #negative sign so we can calculate ratio for RS, don't want negative ratio
+
+    avg_gain = gain.ewm(com = window - 1, min_periods = window).mean() #can't just calculate average gain and loss simply, because recent values matter MORE
+    avg_loss = loss.ewm(com = window - 1, min_periods = window).mean()
+
+    relative_strength = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + relative_strength))
+    return rsi
+
+#MACD: indicates how strong and in what direction a trend for a stock is --> measures momentum
+def macd_calculation(data, short_window = 21, long_window = 50, signal_window = 15): # CANNOT preset window data, since we want to try different windows later
+
+    # calculate exponential moving averages --> we need them for the MACD line
+    short_ema = data['Close'].ewm(com = short_window, adjust = False).mean() #adjust = False because we want weighted averages of the time frame, not of all history
+    long_ema = data['Close'].ewm(com = long_window, adjust = False).mean()
+    macd = short_ema - long_ema
+
+    # calculate the signal line (used to smooth the MACD line)
+    signal_line = macd.ewm(span = signal_window, adjust=False).mean() # if MACD > signal, bullish. if MACD < signal, bearish
+    indicator = macd - signal_line
+    return macd, signal_line, indicator
+
+# Bollinger Bands: measure volatility using the upper band and lower band
+def bollinger_bands_calculation(data, window = 20):
+
+    #calculate the bands (look at google doc for mathematical notation)
+    middle_band = data['Close'].rolling(window).mean()
+    #upper band is middle band + (2 * standard deviations), and lower band is middle band - that
+    #standard deviation changes with rolling window, so we must change the rolling window as well
+    std = data['Close'].rolling(window).std()
+    upper_band = middle_band + (2 * std)
+    lower_band = middle_band - (2 * std)
+    return upper_band, lower_band
+
+def schocastic_oscillator(data, window = 21, smoothing_window = 3):
+    lowest = data['Low'].rolling(window).min()
+    highest = data['High'].rolling(window).max()
+    percent_k = (data["Close"] - lowest)/(highest - lowest) * 100
+
+    # traders smooth percent_k with %D, a 3 day simple-moving average to "smooth out the noise"
+    percent_d = percent_k.rolling(smoothing_window).mean()
+    return percent_d, percent_k
+
+# OBV: the On-Balance Volume (OBV) is a technical indicator that reflects the buying or selling pressure of a stock
+def calculate_OBV(data):
+    # if close of today > close of yesterday you add volume from the yfinance, and vice versa
+    obv = [0] # we have to change OBV into dataframe later
+
+    for i in range(1, len(data)):
+        if data['Close'].iloc[i] > data['Close'].iloc[i - 1]: # our data is from oldest to newest, so this is today - yesterday
+            obv.append(obv[-1] + data["Volume"].iloc[i])  # price up so add volume
+        elif data["Close"].iloc[i] < data["Close"].iloc[i - 1]:
+            obv.append(obv[-1] - data["Volume"].iloc[i])  # price down so minus volume
+        else:
+            obv.append(obv[-1])  # price unchanged so no change in OBV
+    return obv
+
+
+
